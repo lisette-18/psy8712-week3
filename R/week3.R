e84e454e9495651fea6ae332ed4d02dcd0e38bd7
lisette-18 <horne146@umn.edu>
2024-02-04 12:19:26 -0600

    Recast timeStart and timeEnd

--- R/week3.R
+++ R/week3.R
@@ -3,3 +3,5 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
 #Data Import and Cleaning
 raw_df <- read.csv("~/Desktop/PSY 8712/psy8712-week3/data/week3.csv", header = TRUE)
+raw_df$timeStart <- as.POSIXct(raw_df$timeStart)
+raw_df$timeEnd <- as.POSIXct(raw_df$timeEnd)
