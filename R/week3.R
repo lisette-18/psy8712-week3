1a8d546967fec5a3f18baf699a4073db1d461504
lisette-18 <horne146@umn.edu>
2024-02-04 12:30:19 -0600

    Project 3 Completed

--- R/week3.R
+++ R/week3.R
@@ -13,3 +13,6 @@ clean_df$timeSpent <- difftime(clean_df$timeEnd, clean_df$timeStart, units = "se
 hist(as.numeric(clean_df$timeSpent))
 frequency_tables_list <- lapply(clean_df[,5:14], table)
 lapply(frequency_tables_list, barplot)
+sum((clean_df$q1 >= clean_df$q2) & (clean_df$q2 != clean_df$q3))
+for(i in frequency_tables_list) {
+  print(barplot(i))}
