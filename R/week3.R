6545c471aacc1b11565c331abb784131b2de5da2
lisette-18 <horne146@umn.edu>
2024-02-04 12:25:58 -0600

    Created timeSpent variable and histogram

--- R/week3.R
+++ R/week3.R
@@ -7,3 +7,7 @@ raw_df$timeStart <- as.POSIXct(raw_df$timeStart)
 raw_df$timeEnd <- as.POSIXct(raw_df$timeEnd)
 clean_df <- subset(raw_df, raw_df$timeStart > "2017-06-30 23:59:59")
 clean_df <-  subset(clean_df, clean_df$q6==1)
+
+#Analysis
+clean_df$timeSpent <- difftime(clean_df$timeEnd, clean_df$timeStart, units = "secs")
+hist(as.numeric(clean_df$timeSpent))
