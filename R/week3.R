22a5f985a0b816f1adad355a1ea4b9e1fbcbc7d2
lisette-18 <horne146@umn.edu>
2024-02-04 12:17:25 -0600

    Data Import and Cleaning

--- R/week3.R
+++ R/week3.R
@@ -1,2 +1,5 @@
 #Script Settings and Resources
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+
+#Data Import and Cleaning
+raw_df <- read.csv("~/Desktop/PSY 8712/psy8712-week3/data/week3.csv", header = TRUE)
